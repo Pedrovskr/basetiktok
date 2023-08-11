80a59d4444e7f9a71e9d4f56105393ed460e337f
Pedro H <phocunha@gmail.com>
2023-08-11 15:03:17 -0300

    arquivo.py e README.md adicionado

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-BaseTikTok
-Mudanças adicionadas
-
-=======
-# basetiktok
->>>>>>> c1d7fc5d5b220e026d4d932e444ccd8725143841
-=======
-# basetiktok
->>>>>>> c1d7fc5d5b220e026d4d932e444ccd8725143841
+# programa para ler csv
--- arquivo.py
+++ arquivo.py
@@ -5,9 +5,3 @@ import pandas as pd
 df = "https://raw.githubusercontent.com/mateuspestana/datasets_aulas/main/ukraine.csv"
 df2 = pd.read_csv/df
 print(df2)
-=======
-
->>>>>>> c1d7fc5d5b220e026d4d932e444ccd8725143841
-=======
-
->>>>>>> c1d7fc5d5b220e026d4d932e444ccd8725143841
